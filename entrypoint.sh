1075ebab84670aaeafb4fbd7394163c0c5ad37cd
chris ghem enopia <chrisghemenoppia9@gmail.com>
2026-05-21 11:43:57 +0800

    Fix envsubst wiping nginx variables, use sed instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo "PORT is: $PORT"
 php bin/console cache:clear --env=prod
 php bin/console doctrine:migrations:migrate --no-interaction
 
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/nginx.conf
-cp /tmp/nginx.conf /etc/nginx/conf.d/default.conf
+# Use sed instead of envsubst to avoid wiping nginx $variables
+sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
 
 echo "=== nginx config ==="
 cat /etc/nginx/conf.d/default.conf
@@ -19,4 +19,4 @@ php-fpm -D
 sleep 1
 
 echo "Starting nginx..."
-nginx -g "daemon off;" || echo "NGINX FAILED with code $?"
+nginx -g "daemon off;"
