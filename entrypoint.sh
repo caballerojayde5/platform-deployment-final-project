6a7d61af130fe99c76dae9db4e70354713db1392
chris ghem enopia <chrisghemenoppia9@gmail.com>
2026-05-21 11:38:06 +0800

    Fix nginx user to root for Railway

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,16 @@ echo "PORT is: $PORT"
 php bin/console cache:clear --env=prod
 php bin/console doctrine:migrations:migrate --no-interaction
 
-# Substitute $PORT into nginx config
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/nginx.conf
 cp /tmp/nginx.conf /etc/nginx/conf.d/default.conf
 
-# Show the resulting nginx config
 echo "=== nginx config ==="
 cat /etc/nginx/conf.d/default.conf
 
-# Test nginx config
 nginx -t
 
 php-fpm -D
 sleep 1
-nginx -g "daemon off;"
+
+echo "Starting nginx..."
+nginx -g "daemon off;" || echo "NGINX FAILED with code $?"
